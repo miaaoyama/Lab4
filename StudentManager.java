5cd39bb8a740f58067be14ba49ff02e6b16041ce
Marina Tribolet <mia.aoyama@gmail.com>
2026-09-21 22:50:38 -0700

    Add findByName method

--- StudentManager.java
+++ StudentManager.java
@@ -2,4 +2,13 @@ import java.util.ArrayList;
 
 public class StudentManager {
     private ArrayList<Student> students = new ArrayList<>();
+
+    public Student findByName(String name) {
+        for (Student student : students) {
+            if (student.getName().equalsIgnoreCase(name)) {
+                return student;
+            }
+        }
+        return null;
+    }
 }
