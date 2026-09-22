f1ace9c6b64112e66eaa33c152ccf2b4010f6c0e
Francisco Garcia-Vazquez <garcia65230@gmail.com>
2026-09-21 23:46:47 -0700

    This is a commit message for the changes made to the StudentManager.java file. The getStudentCount method has been modified, and a new method getStudentCount2 has been added.

--- StudentManager.java
+++ StudentManager.java
@@ -2,4 +2,12 @@ import java.util.ArrayList;
 
 public class StudentManager {
     private ArrayList<Student> students = new ArrayList<>();
+
+    public int getStudentCount() {
+        return students.size();
+    }
+
+    public void getStudentCount2() {
+
+    }
 }
